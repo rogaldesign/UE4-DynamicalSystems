b13c5c87c3464ff22b2d334c710a64cdf4668b75
Alex Rozgo <alex.rozgo@gmail.com>
2017-11-05 23:00:38 -0600

    significantly expand network bandwidth, use uuid for avatar id

--- Source/DynamicalSystems/Private/NetClient.cpp
+++ Source/DynamicalSystems/Private/NetClient.cpp
@@ -12,6 +12,7 @@ DEFINE_LOG_CATEGORY(RustyNet);
 ANetClient::ANetClient()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	Uuid = FMath::RandRange(10000, 99999);
 }
 
 void ANetClient::RegisterRigidBody(UNetRigidBody* RigidBody)
@@ -22,7 +23,7 @@ void ANetClient::RegisterRigidBody(UNetRigidBody* RigidBody)
 
 void ANetClient::RegisterAvatar(UNetAvatar* _Avatar)
 {
-    UE_LOG(RustyNet, Warning, TEXT("ANetClient::RegisterAvatar %s"), *_Avatar->GetOwner()->GetName());
+    UE_LOG(RustyNet, Warning, TEXT("ANetClient::RegisterAvatar %i %s"), _Avatar->NetID, *_Avatar->GetOwner()->GetName());
     NetAvatars.Add(_Avatar);
 }
 
@@ -63,12 +64,10 @@ void ANetClient::BeginPlay()
     TSharedPtr<class FInternetAddr> localIp = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->GetLocalHostAddr(*GLog, bCanBindAll);
     Local = localIp->ToString(true);
     UE_LOG(RustyNet, Warning, TEXT("GetLocalHostAddr %s"), *Local);
-    
     LastPingTime = UGameplayStatics::GetRealTimeSeconds(GetWorld());
     LastBodyTime = LastPingTime;
     if (Client == NULL) {
         Client = rd_netclient_open(TCHAR_TO_ANSI(*Local), TCHAR_TO_ANSI(*Server), TCHAR_TO_ANSI(*MumbleServer));
-		Uuid = FMath::RandRange(10000, 99999);
         NetClients.Add(Uuid, -1);
         UE_LOG(RustyNet, Warning, TEXT("NetClient BeginPlay %i"), Uuid);
     }
@@ -121,7 +120,8 @@ void ANetClient::Tick(float DeltaTime)
 	{
         TArray<int32> DeleteList;
         for (auto& Elem : NetClients) {
-            if (Elem.Value > 0 && Elem.Value < CurrentTime) {
+            if (Elem.Value > 0 && (CurrentTime - Elem.Value) > 20) {
+				UE_LOG(RustyNet, Warning, TEXT("NetClient DELETED: %i"), Elem.Key);
                 DeleteList.Add(Elem.Key);
             }
         }
@@ -137,9 +137,7 @@ void ANetClient::Tick(float DeltaTime)
 		uint8* bytes = (uint8*)(&Uuid);
 		Msg[1] = bytes[0];
 		Msg[2] = bytes[1];
-		Msg[3] = bytes[2];
-		Msg[4] = bytes[3];
-        rd_netclient_msg_push(Client, Msg, 5);
+        rd_netclient_msg_push(Client, Msg, 3);
         LastPingTime = CurrentTime;
     }
 
@@ -167,7 +165,7 @@ void ANetClient::Tick(float DeltaTime)
                     UStaticMeshComponent* StaticMesh = Actor->FindComponentByClass<UStaticMeshComponent>();
                     if (StaticMesh) {
                         LinearVelocity = StaticMesh->GetBodyInstance()->GetUnrealWorldVelocity();
-                        RigidBodyPack Pack = {(uint8_t)Body->NetID,
+                        RigidBodyPack Pack = {Body->NetID,
                             Location.X, Location.Y, Location.Z, 1,
                             LinearVelocity.X, LinearVelocity.Y, LinearVelocity.Z, 0,
                         };
@@ -188,7 +186,7 @@ void ANetClient::Tick(float DeltaTime)
 			FVector Locations[] = { Avatar->Location, Avatar->LocationHMD, Avatar->LocationHandL, Avatar->LocationHandR };
 			FQuat Rotations[] = { Avatar->Rotation.Quaternion(), Avatar->RotationHMD.Quaternion(), Avatar->RotationHandL.Quaternion(), Avatar->RotationHandR.Quaternion() };
 			for (auto I = 0; I < 4; ++I) {
-				AvatarPack Pack = { (uint8_t)Avatar->NetID,
+				AvatarPack Pack = { Avatar->NetID,
 					Locations[I].X, Locations[I].Y, Locations[I].Z, 1,
 					Rotations[I].X, Rotations[I].Y, Rotations[I].Z, Rotations[I].W,
 				};
@@ -205,80 +203,92 @@ void ANetClient::Tick(float DeltaTime)
         LastBodyTime = CurrentBodyTime;
     }
     
-    RustVec* RustMsg = rd_netclient_msg_pop(Client);
-	uint8* Msg = (uint8*)RustMsg->vec_ptr;
-    if (RustMsg->vec_len > 0) {
+	int Loop = 0;
+	for (; Loop < 1000; Loop += 1) {
+		RustVec* RustMsg = rd_netclient_msg_pop(Client);
+		uint8* Msg = (uint8*)RustMsg->vec_ptr;
+		if (RustMsg->vec_len > 0) {
 
-        if (Msg[0] == 0) { // Ping
-            int32 RemoteUuid = *((int32*)(Msg + 1));
-            NetClients.Add(RemoteUuid, CurrentTime + 5);
-            RebuildConsensus();
-        }
-        else if (Msg[0] == 1) { // World
-            WorldPack* WorldPack = rd_netclient_dec_world(&Msg[1], RustMsg->vec_len - 1);
-            uint64_t NumOfBodies = WorldPack->rigidbodies.vec_len;
-            RigidBodyPack* Bodies = (RigidBodyPack*)WorldPack->rigidbodies.vec_ptr;
-            for (auto Idx=0; Idx<NumOfBodies; ++Idx) {
-                FVector Location(Bodies[Idx].px, (MirrorSyncY ? -1 : 1) * Bodies[Idx].py, Bodies[Idx].pz);
-                FVector LinearVelocity(Bodies[Idx].lx, (MirrorSyncY ? -1 : 1) * Bodies[Idx].ly, Bodies[Idx].lz);
-                uint32_t NetID = Bodies[Idx].id;
-                UNetRigidBody** NetRigidBody = NetRigidBodies.FindByPredicate([NetID](const UNetRigidBody* Item) {
-                    return IsValid(Item) && Item->NetID == NetID;
-                });
-                if (NetRigidBody != NULL && *NetRigidBody != NULL) {
-                    (*NetRigidBody)->SyncTarget = true;
-                    (*NetRigidBody)->TargetLocation = Location;
-                    (*NetRigidBody)->TargetLinearVelocity = LinearVelocity;
-                }
-            }
-            uint64_t NumOfParts = WorldPack->avatarparts.vec_len;
-            if (NumOfParts >= 4) {
-                AvatarPack* Parts = (AvatarPack*)WorldPack->avatarparts.vec_ptr;
-                uint32_t NetID = Parts[0].id;
-                UNetAvatar** NetAvatar = NetAvatars.FindByPredicate([NetID](const UNetAvatar* Item) {
-                    return IsValid(Item) && Item->NetID == NetID;
-                });
-                if (NetAvatar != NULL && *NetAvatar != NULL) {
-                    (*NetAvatar)->LastUpdateTime = CurrentTime;
-                    (*NetAvatar)->Location = FVector(Parts[0].px, Parts[0].py, Parts[0].pz);
-                    (*NetAvatar)->Rotation = FRotator(FQuat(Parts[0].rx, Parts[0].ry, Parts[0].rz, Parts[0].rw));
-                    (*NetAvatar)->LocationHMD = FVector(Parts[1].px, Parts[1].py, Parts[1].pz);
-                    (*NetAvatar)->RotationHMD = FRotator(FQuat(Parts[1].rx, Parts[1].ry, Parts[1].rz, Parts[1].rw));
-                    (*NetAvatar)->LocationHandL = FVector(Parts[2].px, Parts[2].py, Parts[2].pz);
-                    (*NetAvatar)->RotationHandL = FRotator(FQuat(Parts[2].rx, Parts[2].ry, Parts[2].rz, Parts[2].rw));
-                    (*NetAvatar)->LocationHandR = FVector(Parts[3].px, Parts[3].py, Parts[3].pz);
-                    (*NetAvatar)->RotationHandR = FRotator(FQuat(Parts[3].rx, Parts[3].ry, Parts[3].rz, Parts[3].rw));
-                }
-                else {
-                    MissingAvatar = (int)NetID;
-                }
-            }
-            rd_netclient_drop_world(WorldPack);
-        }
-		else if (Msg[0] == 10) { // System Float
-			uint8 MsgSystem = Msg[1];
-			uint8 MsgId = Msg[2];
-			float* MsgValue = (float*)(Msg + 3);
-			UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %f"), Msg[1], Msg[2], *MsgValue);
-			OnSystemFloatMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
+			if (Msg[0] == 0) { // Ping
+				uint16 RemoteUuid = *((uint16*)(Msg + 1));
+				// float* KeyValue = NetClients.Find(RemoteUuid);
+				// if (KeyValue != NULL) {
+				// 	UE_LOG(RustyNet, Warning, TEXT("PING: %i %f"), RemoteUuid, (CurrentTime - (*KeyValue)));
+				// }
+				NetClients.Add(RemoteUuid, CurrentTime);
+				RebuildConsensus();
+			}
+			else if (Msg[0] == 1) { // World
+				WorldPack* WorldPack = rd_netclient_dec_world(&Msg[1], RustMsg->vec_len - 1);
+				uint64_t NumOfBodies = WorldPack->rigidbodies.vec_len;
+				RigidBodyPack* Bodies = (RigidBodyPack*)WorldPack->rigidbodies.vec_ptr;
+				for (auto Idx=0; Idx<NumOfBodies; ++Idx) {
+					FVector Location(Bodies[Idx].px, (MirrorSyncY ? -1 : 1) * Bodies[Idx].py, Bodies[Idx].pz);
+					FVector LinearVelocity(Bodies[Idx].lx, (MirrorSyncY ? -1 : 1) * Bodies[Idx].ly, Bodies[Idx].lz);
+					uint16 NetID = Bodies[Idx].id;
+					UNetRigidBody** NetRigidBody = NetRigidBodies.FindByPredicate([NetID](const UNetRigidBody* Item) {
+						return IsValid(Item) && Item->NetID == NetID;
+					});
+					if (NetRigidBody != NULL && *NetRigidBody != NULL) {
+						(*NetRigidBody)->SyncTarget = true;
+						(*NetRigidBody)->TargetLocation = Location;
+						(*NetRigidBody)->TargetLinearVelocity = LinearVelocity;
+					}
+				}
+				uint64_t NumOfParts = WorldPack->avatarparts.vec_len;
+				if (NumOfParts >= 4) {
+					AvatarPack* Parts = (AvatarPack*)WorldPack->avatarparts.vec_ptr;
+					uint16 NetID = Parts[0].id;
+					UNetAvatar** NetAvatar = NetAvatars.FindByPredicate([NetID](const UNetAvatar* Item) {
+						return IsValid(Item) && Item->NetID == NetID;
+					});
+					if (NetAvatar != NULL && *NetAvatar != NULL) {
+						(*NetAvatar)->LastUpdateTime = CurrentTime;
+						(*NetAvatar)->Location = FVector(Parts[0].px, Parts[0].py, Parts[0].pz);
+						(*NetAvatar)->Rotation = FRotator(FQuat(Parts[0].rx, Parts[0].ry, Parts[0].rz, Parts[0].rw));
+						(*NetAvatar)->LocationHMD = FVector(Parts[1].px, Parts[1].py, Parts[1].pz);
+						(*NetAvatar)->RotationHMD = FRotator(FQuat(Parts[1].rx, Parts[1].ry, Parts[1].rz, Parts[1].rw));
+						(*NetAvatar)->LocationHandL = FVector(Parts[2].px, Parts[2].py, Parts[2].pz);
+						(*NetAvatar)->RotationHandL = FRotator(FQuat(Parts[2].rx, Parts[2].ry, Parts[2].rz, Parts[2].rw));
+						(*NetAvatar)->LocationHandR = FVector(Parts[3].px, Parts[3].py, Parts[3].pz);
+						(*NetAvatar)->RotationHandR = FRotator(FQuat(Parts[3].rx, Parts[3].ry, Parts[3].rz, Parts[3].rw));
+					}
+					else {
+						MissingAvatar = (int)NetID;
+						UE_LOG(RustyNet, Warning, TEXT("NetClient MissingAvatar: %i"), NetID);
+					}
+				}
+				rd_netclient_drop_world(WorldPack);
+			}
+			else if (Msg[0] == 10) { // System Float
+				uint8 MsgSystem = Msg[1];
+				uint8 MsgId = Msg[2];
+				float* MsgValue = (float*)(Msg + 3);
+				UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %f"), Msg[1], Msg[2], *MsgValue);
+				OnSystemFloatMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
+			}
+			else if (Msg[0] == 11) { // System Int
+				uint8 MsgSystem = Msg[1];
+				uint8 MsgId = Msg[2];
+				int32* MsgValue = (int32*)(Msg + 3);
+				UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %i"), Msg[1], Msg[2], *MsgValue);
+				OnSystemIntMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
+			}
+			else if (Msg[0] == 12) { // System String
+				uint8 MsgSystem = Msg[1];
+				uint8 MsgId = Msg[2];
+				const char* MsgValuePtr = (const char*)(Msg + 3);
+				FString MsgValue(MsgValuePtr);
+				UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %s"), Msg[1], Msg[2], *MsgValue);
+				OnSystemStringMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
+			}
 		}
-		else if (Msg[0] == 11) { // System Int
-			uint8 MsgSystem = Msg[1];
-			uint8 MsgId = Msg[2];
-			int32* MsgValue = (int32*)(Msg + 3);
-			UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %i"), Msg[1], Msg[2], *MsgValue);
-			OnSystemIntMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
-		}
-		else if (Msg[0] == 12) { // System String
-			uint8 MsgSystem = Msg[1];
-			uint8 MsgId = Msg[2];
-			const char* MsgValuePtr = (const char*)(Msg + 3);
-			FString MsgValue(MsgValuePtr);
-			UE_LOG(RustyNet, Warning, TEXT("Msg IN MsgSystem: %u MsgId: %u MsgValue: %s"), Msg[1], Msg[2], *MsgValue);
-			OnSystemStringMsg.Broadcast(MsgSystem, MsgId, *MsgValue);
+		rd_netclient_msg_drop(RustMsg);
+		if (RustMsg->vec_len == 0) {
+			//UE_LOG(RustyNet, Warning, TEXT("NetClient Loop: %i"), Loop);
+			break;
 		}
 	}
-	rd_netclient_msg_drop(RustMsg);
 
     
     //RustVec* RustVox = rd_netclient_vox_pop(Client);
